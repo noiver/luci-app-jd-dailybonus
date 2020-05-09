22b517390a1756148954626df8884ab1005cfaec
jerrykuku <jerrykuku@qq.com>
2020-05-09 18:33:35 +0800

    Update jd-dailybonus.lua

--- luasrc/controller/jd-dailybonus.lua
+++ luasrc/controller/jd-dailybonus.lua
@@ -50,8 +50,10 @@ function run()
         luci.sys.call(cmd3_1)
         luci.sys.call(cmd4)
         mod_script(cookie,cookie2)
-        luci.sys.call("nohup node /usr/share/jd-dailybonus/JD_DailyBonus.js >/www/JD_DailyBonus.htm 2>/dev/null &")
-        
+        local date = os.date("%Y-%m-%d %H:%M:%S", os.time())
+        luci.sys.call("echo '".. date .."' >/www/JD_DailyBonus.htm 2>/dev/null")
+        luci.sys.call("nohup node /usr/share/jd-dailybonus/JD_DailyBonus.js >>/www/JD_DailyBonus.htm 2>/dev/null &")
+        luci.sys.call("echo 'end sign' >>/www/JD_DailyBonus.htm 2>/dev/null")
         e.error = 0
     else
         e.error = 1
